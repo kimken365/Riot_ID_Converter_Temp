60c20acdce66dc0a56a53d60918b0bc28261cbf3
kenkim365 <kimkenneth114@gmail.com>
2024-07-03 01:19:11 -0400

    rbh only

--- Riot_ID_Converter/Riot_ID_Converter.py
+++ Riot_ID_Converter/Riot_ID_Converter.py
@@ -7,52 +7,33 @@ def user_input():
     #user_input = input("Type Y to include alts. Otherwise press enter: ")
     #return user_input
 
-def riot_id_converter(riot_id_table, include_alts):
-    #copies from clipboard
+def riot_id_converter():
+    # Copies from clipboard
     input_text = pyperclip.paste()
     output_lines = []
 
     for line in input_text.split('\n'):
         modified_line = line.strip()
 
-        #users with ALTS
-        for riot_id, suffix_list in riot_accounts_table.items():
-            
-            if riot_id in line:
-                # include all the accounts of the users
-                if include_alts != 'Y':
-                    modified_line = '\n'.join(suffix_list)
-                # includes only the main account of the users
-                else:
-                    modified_line = suffix_list[0]
-    
-                output_lines.append(modified_line)
-                break
+        
+        
+        colon_positions = [pos for pos, char in enumerate(modified_line) if char == ':']
+        fourth_colon_position = colon_positions[3]
+        modified_line = modified_line[fourth_colon_position + 1:].strip()
+        output_lines.append(modified_line)
+           
+        
+ 
 
-                    
-        #users without ALTS  
-        else:
-            if ':' in modified_line(':') >= 2:
-                first_colon_index = modified_line.find(':')
-                second_colon_index = modified_line.find(':', first_colon_index + 1)
-                modified_line = modified_line[second_colon_index + 1:]
-            else:
-                modified_line = modified_line[3:] 
-                if modified_line and modified_line[0].isspace():
-                    modified_line = modified_line[2:]
-                output_lines.append(modified_line)
+    # Join the output lines into a single string and copy to clipboard
+    output_text = '\n'.join(output_lines)
+    pyperclip.copy(output_text)
             
         
     #saves to clipboard
     output_text = '\n'.join(output_lines)
     pyperclip.copy(output_text)
 
-#add new usernames here
-riot_accounts_table = {
-    'Suyeonchan': ['Suyeonchan#00000',
-                   ],    
-}
 
-include_alts = user_input()
 
-riot_id_converter(riot_accounts_table, include_alts)
+riot_id_converter()
